f67bde67ece3fd65b07d570d07d1c4e4806fe822
Hieu luong <hieuluong290354@gmail.com>
2024-12-10 12:47:46 +0700

    Thay đổi biểu đồ lớp

--- DataBase.sql
+++ DataBase.sql
@@ -17,17 +17,6 @@ create table NguoiDung (
 );
 go
 
--- Bảng nhà tài trợ
-create table NhaTaiTro (
-    IDNhaTaiTro varchar(10) primary key not null, 
-    TenNhaTaiTro nvarchar(100) not null,
-    Email varchar(50) not null unique, 
-    SoDienThoai varchar(10) not null unique,
-    DiaChi nvarchar(200) null, 
-    GhiChu nvarchar(200) null 
-);
-go
-
 -- Bảng loại sự kiện
 create table LoaiSuKien (
     IDLoaiSuKien varchar(10) primary key not null, 
@@ -51,16 +40,6 @@ create table SuKien (
     MucDich nvarchar(50) not null,
 	QuyDinh nvarchar(20) not null check (QuyDinh in (N'Phải mua vé', N'Không phải mua vé')),
 	TrangThaiSuKien nvarchar(20) default N'Chờ xác nhận' check (TrangThaiSuKien in (N'Chờ xác nhận',N'Đã xác nhận' ,N'Chưa bắt đầu', N'Đang diễn ra', N'Hoàn thành', N'Hủy')));
-go
-
--- Bảng tài trợ
-create table PhieuTaiTroSuKien (
-    IDTaiTro varchar(10) primary key not null, 
-    IDNhaTaiTro varchar(10) foreign key references NhaTaiTro(IDNhaTaiTro) not null, 
-    IDSuKien varchar(10) foreign key references SuKien(IDSuKien) not null,
-    SoTienTaiTro float not null, 
-    NgayTaiTro datetime default getdate() not null
-);
 go
 
 -- Bảng Vé
@@ -104,9 +83,9 @@ create table NhanVien (
 );
 go
 
--- Bảng Phản hồi
-create table PhanHoi (
-    IDPhanHoi varchar(10) primary key not null,
+-- Bảng Đánh giá
+create table DanhGia (
+    IDDanhGia varchar(10) primary key not null,
     IDSuKien varchar(10) foreign key references SuKien(IDSuKien) not null,
     IDNguoiDung varchar(10) foreign key references NguoiDung(IDNguoiDung) not null,
     DanhGia int not null,
@@ -120,10 +99,8 @@ DROP TABLE IF EXISTS PhanHoi;
 DROP TABLE IF EXISTS ThanhToan;
 DROP TABLE IF EXISTS NhanVien;
 DROP TABLE IF EXISTS Ve;
-DROP TABLE IF EXISTS TaiTroSuKien;
 DROP TABLE IF EXISTS SuKien;
 DROP TABLE IF EXISTS LoaiSuKien;
-DROP TABLE IF EXISTS NhaTaiTro;
 DROP TABLE IF EXISTS NhomNhanVien;
 DROP TABLE IF EXISTS NguoiDung;
 GO
